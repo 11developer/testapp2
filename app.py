1c71a090e2ed2f4e0cf95a9d9952fd5052f3674f
root <developer11@programmer.net>
2020-09-30 11:51:38 +0300

    Updated

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 #---- ---- ---- ----
-#After importing SQLAlchemy and bind it to our flask app, we can declare our models. Here we declare model called Data and defined its field with it's properties.
-class Data(db.Model):
+#After importing SQLAlchemy and bind it to our flask app, we can declare our models. Here we declare model called eData and defined its field with it's properties.
+class eData(db.Model):
     __tablename__ = 'records'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=False)
@@ -39,35 +39,35 @@ class Data(db.Model):
         #self.timestamp = timestamp
 #---- ---- ---- ----
 #This part defined structure of response of our endpoint. We want that all of our endpoint will have JSON response. Here we define that our JSON response will have two keys(username, and email). Also we defined user_schema as instance of UserSchema, and user_schemas as instances of list of UserSchema.
-class DataSchema(ma.Schema):
+class eDataSchema(ma.Schema):
     class Meta:
         # Fields to expose
 	# fields = ('username', 'email')
         #fields = ('id', 'username', 'email')
         fields = ('id', 'username', 'email', 'timestamp')
 
-data_schema = DataSchema()
-datas_schema = DataSchema(many=True)
+data_schema = eDataSchema()
+datas_schema = eDataSchema(many=True)
 #---- ---- ---- ----
 #---- ---- ---- ----
 # endpoint to show all records
 @app.route("/data", methods=["GET"])
 def get_data():
-    all_data = Data.query.all()
+    all_data = eData.query.all()
     result = datas_schema.dump(all_data)
     return datas_schema.jsonify(all_data)
 #---- ---- ---- ----
 # endpoint to get user detail by id
 @app.route("/data/<id>", methods=["GET"])
 def data_detail_id(id):
-    data = Data.query.get(id)
+    data = eData.query.get(id)
     return data_schema.jsonify(data)
 #---- ---- ---- ----
 # endpoint to get user detail by username
 @app.route("/data/username/<username>", methods=["GET"])
 def data_detail_username(username):
-    #data = Data.query.get(id)
-    data = Data.query.filter(Data.username == username).one_or_none()
+    #data = eData.query.get(id)
+    data = eData.query.filter(eData.username == username).one_or_none()
     return data_schema.jsonify(data)
 #---- ---- ---- ----
 #endpoint to create new record
@@ -78,8 +78,8 @@ def add_data():
     username = request.json['username']
     email = request.json['email']
     
-    new_data = Data(username, email)
-    if db.session.query(Data).filter_by(username=username).count() > 0:
+    new_data = eData(username, email)
+    if db.session.query(eData).filter_by(username=username).count() > 0:
         return jsonify('This data exist on database!')
 
     db.session.add(new_data)
@@ -89,7 +89,7 @@ def add_data():
 # endpoint to update a record
 @app.route("/data/<id>", methods=["PUT"])
 def data_update(id):
-    data = Data.query.get(id)
+    data = eData.query.get(id)
 	#id = request.json['id']
     username = request.json['username']
     email = request.json['email']
@@ -104,7 +104,7 @@ def data_update(id):
 # endpoint to delete a record
 @app.route("/data/<id>", methods=["DELETE"])
 def data_delete(id):
-    data = Data.query.get(id)
+    data = eData.query.get(id)
     db.session.delete(data)
     db.session.commit()
 
